27f502daf863de3cdff7a1517e3e0187b0a4117e
shanicejessica <31205863+shanicejessica@users.noreply.github.com>
2017-09-01 09:57:32 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number;
+number=34;
+cout<<"numb="<<number<<endl;
+double a,b,multiply,divide;
+a=3.7654;
+b=3.9875;
+multiply=a*b;
+cout<<"product="<<multiply<<endl;
+divide=a/b;
+cout<<"quotient="<<divide<<endl;
+char d;
+d='s';
+cout<<"name="<<d;
+bool odd=false;
+int z;
+cout<<"enter no-";
+cin>>z;
+if(z%2==1) odd=true;
+if(odd==true) cout<<"odd";
+else
+cout<<"even";
+return 0;
+}
+}
